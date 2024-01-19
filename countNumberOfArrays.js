16af1a15ce99359e460ff1cce84c30633561fe67
Desmond Stular <dstular@yahoo.com>
2024-01-19 11:32:35 -0700

    Added comments to countNumberOfArrays.js

--- countNumberOfArrays.js
+++ countNumberOfArrays.js
@@ -1,3 +1,11 @@
+/**
+ * countNumberOfArrays(array)
+ * Counts and returns the number of arrays nested in an array
+ * that is passed as a parameter.
+ *
+ * @param array - An array of values
+ * @returns {number} - Number of arrays found
+ */
 const countNumberOfArrays = function (array) {
     let result = 0;
     array.forEach(function(element) {
