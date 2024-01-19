65ef2300a78d0afe0fd3c5e50ad940eb2ff50f44
Desmond Stular <dstular@yahoo.com>
2024-01-19 12:14:32 -0700

    Added winnersObject.js which returns object of players and those they beat

---
+++ winnersObject.js
@@ -0,0 +1,29 @@
+/**
+ * winnersObject(matches)
+ * Creates an object containing each player's name as a key
+ * and the an array of names of those they beat. Accepts an
+ * array of matches as a parameter.
+ *
+ * @param matches - An array of matches
+ * @returns {{}} - An object of participants names and those they beat.
+ */
+const winnersObject = function (matches) {
+    let winners = {};
+    matches.forEach(function (value) {
+        // Add winner name to winners object if not already
+        if (!(value.winner in winners)) {
+            winners[value.winner] = [];
+        }
+        // Add loser name to winners object if not already
+        if (!(value.loser in winners)) {
+            winners[value.loser] = [];
+        }
+        // If loser name not found already in winner array, add name
+        if (winners[value.winner].indexOf(value.loser) === -1) {
+            winners[value.winner].push(value.loser);
+        }
+    })
+    return winners;
+}
+
+module.exports = winnersObject;
