b205d2d43dda42173fda768375855f06a0c2c843
Desmond Stular <dstular@yahoo.com>
2024-01-19 11:22:16 -0700

    Added isArrayFactorChain.js which checks if a passed array is a factor chain.

---
+++ isArrayFactorChain.js
@@ -0,0 +1,21 @@
+/**
+ * isArrayFactorChain(array)
+ * Checks an array of numbers to see if it is an array factor
+ * chain.
+ *
+ * @param array - An array of numerical values.
+ * @returns {boolean} - True if factor chain, false if not.
+ */
+const isArrayFactorChain = function (array) {
+    if (array.length < 2) {  // not factor if array < 2 values
+        return false;
+    }
+    for (let i= 0; i < array.length - 1; i++) {
+        if (array[i+1] % array[i] !== 0) {
+            return false;   // return false if next element not facto
+        }
+    }
+    return true;    // If loop completes, a factor chain
+};
+
+module.exports = isArrayFactorChain;
