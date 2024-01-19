ac1f91fd04cd08d90072010ae88dac4080da866c
Desmond Stular <dstular@yahoo.com>
2024-01-19 12:44:45 -0700

    Created index.js to allow module exporting.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// Array functions
+const countNumberOfArrays = require('./countNumberOfArrays');
+const findNaNIndex = require('./findNaNIndex');
+const isArrayFactorChain = require('./isArrayFactorChain');
+
+// Object functions
+const biggestLoser = require('./biggestLoser');
+const participants = require('./participants');
+const winnersObject = require('./winnersObject');
+
+module.exports = {
+    countNumberOfArrays,
+    findNaNIndex,
+    isArrayFactorChain,
+    biggestLoser,
+    participants,
+    winnersObject,
+};
